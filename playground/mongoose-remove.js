425308780c8c2887ca70d05b493a84ed826e8806
Gidi Shwartzman <gshwartzman@paloaltonetworks.com>
2017-08-27 16:17:34 +0300

    Node: Added delete by id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,16 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+// Todo.findOneAndRemove()
+// Todo.findByIdAndRemove()
+
+Todo.findByIdAndRemove('59a2bec91ba72afbcf6bfa09').then((todo) => {
+  console.log(todo);
+})
