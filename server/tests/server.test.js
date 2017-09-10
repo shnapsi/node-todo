2fd088cd928b1246e9ec72c301b099e263ea9573
Gidi Shwartzman <gshwartzman@paloaltonetworks.com>
2017-09-10 11:03:20 +0300

    Node: Added test case for /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -220,7 +220,7 @@ describe('Delete /todos :id', () => {
               expect(user).toExist();
               expect(user.password).toNotBe(password);
               done();
-            });
+            }).catch((e) => done(e));
           });
       });
 
@@ -246,6 +246,49 @@ describe('Delete /todos :id', () => {
         request(app)
           .post('/users')
           .send({email, password})
+          .expect(400)
+          .expect((res) => {
+            expect(res.headers['x-auth']).toNotExist();
+            expect(res.body._id).toNotExist();
+          })
+          .end(done);
+      });
+    });
+
+    describe('POST /users/login', () => {
+      it('should login user and return auth token', (done) => {
+        request(app)
+          .post('/users/login')
+          .send({
+            email: users[1].email,
+            password: users[1].password
+          })
+          .expect(200)
+          .expect((res) => {
+            expect(res.headers['x-auth']).toExist();
+          })
+          .end((err, res) => {
+            if(err) {
+              return done(err);
+            }
+            User.findById(users[1]._id).then((user) => {
+              expect(user.tokens[0]).toInclude( {
+                access: 'auth',
+                token: res.headers['x-auth']
+              });
+                done();
+            }).catch((e) => done(e));
+          });
+
+      });
+
+      it('should reject invalid login', (done) => {
+        request(app)
+          .post('/users/login')
+          .send({
+            email: users[1].email,
+            password: users[1].password + 'add'
+          })
           .expect(400)
           .expect((res) => {
             expect(res.headers['x-auth']).toNotExist();
